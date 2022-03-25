180050285051ef65689ef895a77f2b810f80c658
Ljubisav <20ljubisav.ceranic01@gmail.com>
2022-03-26 00:39:00 +0100

    sta

--- pages/test-1.vue
+++ pages/test-1.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <h1>Home page</h1>
-    <NuxtLink to="/about">
+    <NuxtLink to="test-2">
       About (internal link that belongs to the Nuxt App)
     </NuxtLink>
     <a href="https://youtube.com">vodi do youtube</a>
---
+++ pages/test-2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="https://www.w3schools.com/images/picture.jpg" alt="Mountain">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
